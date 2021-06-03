09280bcfb6580c7253568d8e8aa53dde92cfbcbc
Ivan <ichang@broadinstitute.org>
2021-06-03 12:13:36 -0400

    Add Deploy step to workflow.

--- src/main/java/bio/terra/cloudfunctions/streaming/GcsBQ.java
+++ src/main/java/bio/terra/cloudfunctions/streaming/GcsBQ.java
@@ -12,6 +12,7 @@ public class GcsBQ implements BackgroundFunction<GCSEvent> {
 
   @Override
   public void accept(GCSEvent event, Context context) throws Exception {
+    System.out.println("Event: " + context.eventId());
     logger.info("Event: " + context.eventId());
     logger.info("Event Type: " + context.eventType());
     logger.info("Bucket: " + event.getBucket());
