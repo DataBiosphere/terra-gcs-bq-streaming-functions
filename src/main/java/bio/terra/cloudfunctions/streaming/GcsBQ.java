aec994bb863d218cd895bb61304621c280b799a4
Ivan <ichang@broadinstitute.org>
2021-06-04 08:56:21 -0400

    Add envvar GCLOUD_PROJECT to Java 11 runtime.

--- src/main/java/bio/terra/cloudfunctions/streaming/GcsBQ.java
+++ src/main/java/bio/terra/cloudfunctions/streaming/GcsBQ.java
@@ -3,10 +3,16 @@ package bio.terra.cloudfunctions.streaming;
 import bio.terra.cloudevents.GCSEvent;
 import com.google.cloud.functions.BackgroundFunction;
 import com.google.cloud.functions.Context;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageException;
+import com.google.cloud.storage.StorageOptions;
 import java.io.BufferedInputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Logger;
 import org.apache.commons.compress.archivers.ArchiveEntry;
 import org.apache.commons.compress.archivers.ArchiveInputStream;
@@ -47,5 +53,35 @@ public class GcsBQ implements BackgroundFunction<GCSEvent> {
     while ((archiveEntry = archiveInputStream.getNextEntry()) != null) {
       logger.info(archiveEntry.getName() + " " + archiveEntry.getSize() + " bytes");
     }
+  }
+
+  /**
+   * Signing a URL requires Credentials which implement ServiceAccountSigner. These can be set
+   * explicitly using the Storage.SignUrlOption.signWith(ServiceAccountSigner) option. If you don't,
+   * you could also pass a service account signer to StorageOptions, i.e.
+   * StorageOptions().newBuilder().setCredentials(ServiceAccountSignerCredentials). In this example,
+   * neither of these options are used, which means the following code only works when the
+   * credentials are defined via the environment variable GOOGLE_APPLICATION_CREDENTIALS, and those
+   * credentials are authorized to sign a URL. See the documentation for Storage.signUrl for more
+   * details.
+   */
+  private void generateV4GetObjectSignedUrl(String projectId, String bucketName, String objectName)
+      throws StorageException {
+    // String projectId = "my-project-id";
+    // String bucketName = "my-bucket";
+    // String objectName = "my-object";
+
+    Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+
+    // Define resource
+    BlobInfo blobInfo = BlobInfo.newBuilder(BlobId.of(bucketName, objectName)).build();
+
+    URL url =
+        storage.signUrl(blobInfo, 5, TimeUnit.MINUTES, Storage.SignUrlOption.withV4Signature());
+
+    logger.info("Generated GET signed URL:");
+    logger.info(url.toString());
+    logger.info("You can use this URL with any user agent, for example:");
+    logger.info("curl '" + url + "'");
   }
 }
