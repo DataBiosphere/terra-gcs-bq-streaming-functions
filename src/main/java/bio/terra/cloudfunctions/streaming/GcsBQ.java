6edee226834571fdefc24d5cdc1bbed2a683b4f1
Ivan <ichang@broadinstitute.org>
2021-06-03 23:32:01 -0400

    Add decompress steps.

--- src/main/java/bio/terra/cloudfunctions/streaming/GcsBQ.java
+++ src/main/java/bio/terra/cloudfunctions/streaming/GcsBQ.java
@@ -3,8 +3,16 @@ package bio.terra.cloudfunctions.streaming;
 import bio.terra.cloudfunctions.streaming.GcsBQ.GCSEvent;
 import com.google.cloud.functions.BackgroundFunction;
 import com.google.cloud.functions.Context;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Date;
 import java.util.logging.Logger;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.ArchiveInputStream;
+import org.apache.commons.compress.archivers.ArchiveStreamFactory;
+import org.apache.commons.compress.compressors.CompressorInputStream;
+import org.apache.commons.compress.compressors.CompressorStreamFactory;
 
 public class GcsBQ implements BackgroundFunction<GCSEvent> {
   private static final Logger logger = Logger.getLogger(GcsBQ.class.getName());
@@ -14,7 +22,28 @@ public class GcsBQ implements BackgroundFunction<GCSEvent> {
     logger.info("Event: " + context.eventId());
     logger.info("Event Type: " + context.eventType());
     logger.info(event.toString());
-    throw new UnsupportedOperationException("Not supported yet.");
+
+    InputStream in = new URL(event.getMediaLink()).openStream();
+    CompressorStreamFactory compressor = CompressorStreamFactory.getSingleton();
+    CompressorInputStream uncompressedInputStream =
+        in.markSupported()
+            ? compressor.createCompressorInputStream(in)
+            : compressor.createCompressorInputStream(new BufferedInputStream(in));
+
+    ArchiveStreamFactory archiver = new ArchiveStreamFactory();
+    ArchiveInputStream archiveInputStream =
+        uncompressedInputStream.markSupported()
+            ? archiver.createArchiveInputStream(uncompressedInputStream)
+            : archiver.createArchiveInputStream(new BufferedInputStream(uncompressedInputStream));
+
+    // ArchiveInputStream is a special type of InputStream that emits an EOF when it gets to the end
+    // of a file in the archive.
+    // Once it’s done, call getNextEntry to reset the stream and start reading the next file.
+    // When getNextEntry returns null, you’re at the end of the archive.
+    ArchiveEntry archiveEntry;
+    while ((archiveEntry = archiveInputStream.getNextEntry()) != null) {
+      logger.info(archiveEntry.getName() + " " + archiveEntry.getSize() + " bytes");
+    }
   }
 
   public static class GCSEvent {
@@ -264,35 +293,77 @@ public class GcsBQ implements BackgroundFunction<GCSEvent> {
 
     @Override
     public String toString() {
-      return "GCSEvent{" +
-              "id='" + id + '\'' +
-              ", selfLink='" + selfLink + '\'' +
-              ", name='" + name + '\'' +
-              ", bucket='" + bucket + '\'' +
-              ", generation=" + generation +
-              ", metageneration=" + metageneration +
-              ", contentType='" + contentType + '\'' +
-              ", timeCreated=" + timeCreated +
-              ", updated=" + updated +
-              ", customTime=" + customTime +
-              ", timeDeleted=" + timeDeleted +
-              ", temporaryHold=" + temporaryHold +
-              ", eventBasedHold=" + eventBasedHold +
-              ", retentionExpirationTime=" + retentionExpirationTime +
-              ", storageClass='" + storageClass + '\'' +
-              ", timeStorageClassUpdated=" + timeStorageClassUpdated +
-              ", size=" + size +
-              ", md5Hash='" + md5Hash + '\'' +
-              ", mediaLink='" + mediaLink + '\'' +
-              ", contentEncoding='" + contentEncoding + '\'' +
-              ", contentDisposition='" + contentDisposition + '\'' +
-              ", contentLanguage='" + contentLanguage + '\'' +
-              ", cacheControl='" + cacheControl + '\'' +
-              ", crc32c='" + crc32c + '\'' +
-              ", componentCount=" + componentCount +
-              ", etag='" + etag + '\'' +
-              ", kmsKeyName='" + kmsKeyName + '\'' +
-              '}';
+      return "GCSEvent{"
+          + "id='"
+          + id
+          + '\''
+          + ", selfLink='"
+          + selfLink
+          + '\''
+          + ", name='"
+          + name
+          + '\''
+          + ", bucket='"
+          + bucket
+          + '\''
+          + ", generation="
+          + generation
+          + ", metageneration="
+          + metageneration
+          + ", contentType='"
+          + contentType
+          + '\''
+          + ", timeCreated="
+          + timeCreated
+          + ", updated="
+          + updated
+          + ", customTime="
+          + customTime
+          + ", timeDeleted="
+          + timeDeleted
+          + ", temporaryHold="
+          + temporaryHold
+          + ", eventBasedHold="
+          + eventBasedHold
+          + ", retentionExpirationTime="
+          + retentionExpirationTime
+          + ", storageClass='"
+          + storageClass
+          + '\''
+          + ", timeStorageClassUpdated="
+          + timeStorageClassUpdated
+          + ", size="
+          + size
+          + ", md5Hash='"
+          + md5Hash
+          + '\''
+          + ", mediaLink='"
+          + mediaLink
+          + '\''
+          + ", contentEncoding='"
+          + contentEncoding
+          + '\''
+          + ", contentDisposition='"
+          + contentDisposition
+          + '\''
+          + ", contentLanguage='"
+          + contentLanguage
+          + '\''
+          + ", cacheControl='"
+          + cacheControl
+          + '\''
+          + ", crc32c='"
+          + crc32c
+          + '\''
+          + ", componentCount="
+          + componentCount
+          + ", etag='"
+          + etag
+          + '\''
+          + ", kmsKeyName='"
+          + kmsKeyName
+          + '\''
+          + '}';
     }
   }
 }
