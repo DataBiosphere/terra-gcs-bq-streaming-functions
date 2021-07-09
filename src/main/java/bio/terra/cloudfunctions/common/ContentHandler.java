a1620cc6c392f8b963af03bf6e39970e03d1343b
Ivan <ichang@broadinstitute.org>
2021-07-09 15:06:26 -0400

    Add comments to public methods of ContentHandler.

--- src/main/java/bio/terra/cloudfunctions/common/ContentHandler.java
+++ src/main/java/bio/terra/cloudfunctions/common/ContentHandler.java
@@ -4,17 +4,23 @@ import com.google.events.cloud.storage.v1.StorageObjectData;
 
 public abstract class ContentHandler<E extends StorageObjectData> extends FileTypeDetector<E> {
   public ContentHandler() {}
-
+  /**
+   * The translate method is to be overridden in inherited class that will transform the raw
+   * dataStream into domain-specific format before further processing.
+   */
   public void translate() throws Exception {
     throw new UnsupportedOperationException("translate method must be overridden by sub-classes");
   }
-
+  /**
+   * This handleMediaType method calls the base class method then delegates to the domain-specific
+   * translator.
+   */
   @Override
   public void handleMediaType() throws Exception {
     super.handleMediaType();
     translate();
   }
-
+  /** This insert method is used for inserting the translated data into BigQuery. */
   public void insert() throws Exception {
     throw new UnsupportedOperationException("insert method must be overridden by sub-classes");
   }
