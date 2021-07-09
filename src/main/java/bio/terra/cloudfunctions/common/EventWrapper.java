4360bc94b9287bd5654f80e8704bda9ac0c71ad0
Ivan <ichang@broadinstitute.org>
2021-07-09 14:56:31 -0400

    Add comments to public methods of EventWrapper.

--- src/main/java/bio/terra/cloudfunctions/common/EventWrapper.java
+++ src/main/java/bio/terra/cloudfunctions/common/EventWrapper.java
@@ -3,7 +3,14 @@ package bio.terra.cloudfunctions.common;
 import org.apache.commons.lang3.StringUtils;
 
 public class EventWrapper<T> {
-  public static <T> T parseEvent(String json, Class<T> type) {
+  /**
+   * Parse an event String and returns the event represented by generic event type E.
+   *
+   * @param json the event String
+   * @param type the type of the event
+   * @return object representing event type E
+   */
+  public static <E> E parseEvent(String json, Class<E> type) {
     assert (StringUtils.isNotBlank(json));
     return GsonWrapper.getInstance().fromJson(json, type);
   }
