800ebaed6fdd99d48ccd6e72305e3fdf33d13ed2
Ivan <ichang@broadinstitute.org>
2021-07-15 13:30:36 -0400

    Added a harness for Cloud Function events.

---
+++ src/main/java/bio/terra/cloudfunctions/streaming/GenericCFEventHandler.java
@@ -0,0 +1,17 @@
+package bio.terra.cloudfunctions.streaming;
+
+import bio.terra.cloudfunctions.common.CloudEventsHarness;
+import io.cloudevents.CloudEvent;
+import java.util.logging.Logger;
+
+public class GenericCFEventHandler extends CloudEventsHarness {
+  private static final Logger logger = Logger.getLogger(GenericCFEventHandler.class.getName());
+
+  @Override
+  public void accept(CloudEvent event) throws Exception {
+    super.accept(event);
+    logger.info("Type: " + event.getType());
+    logger.info("DataContentType: " + event.getDataContentType());
+    logger.info("Source: " + event.getSource().toString());
+  }
+}
