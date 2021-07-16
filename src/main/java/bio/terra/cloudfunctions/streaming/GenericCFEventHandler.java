34108fdccc3a43edc3130b11d0d1aec3e9109deb
Ivan <ichang@broadinstitute.org>
2021-07-15 20:12:50 -0400

    Added checks for event type.

--- src/main/java/bio/terra/cloudfunctions/streaming/GenericCFEventHandler.java
+++ src/main/java/bio/terra/cloudfunctions/streaming/GenericCFEventHandler.java
@@ -17,7 +17,8 @@ public class GenericCFEventHandler extends CloudEventsHarness {
     logger.info("TypeName: " + event.getClass().getTypeName());
     logger.info("Test 1: " + CloudEventV1.class.isInstance(event));
     logger.info("Test 2: " + event.getClass());
-    logger.info("Data Schema: " + event.getDataSchema().toString());
+    logger.info("Test 3: " + CloudEventV1.class);
+    logger.info("Data Schema: " + event.getDataSchema());
     if (CloudEventV1.class.isInstance(event)) {
       if (event.getType().equals("google.cloud.storage.object.v1.finalized")) {
         logger.info("Data TypeName: " + event.getData().getClass().getTypeName());
