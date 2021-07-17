340a41f90729e83977a3efad5f46734f2a4b3e34
Ivan <ichang@broadinstitute.org>
2021-07-17 12:37:06 -0400

    Test generic type cast.

--- src/main/java/bio/terra/cloudfunctions/common/CloudEventsHarness.java
+++ src/main/java/bio/terra/cloudfunctions/common/CloudEventsHarness.java
@@ -87,7 +87,7 @@ public abstract class CloudEventsHarness implements CloudEventsFunction {
                 event.getData().toBytes(),
                 d -> GsonWrapper.getInstance().fromJson(new String(d), StorageObjectData.class));
         messageType = StorageObjectEventMessage.class;
-        logger.info("Concrete event data: " + StorageObjectEventMessage.class.cast(message).getMessage());
+        logger.info("Concrete event data: " + StorageObjectEventMessage.class.cast(message).getMessage().toString());
         break;
       default:
         break;
