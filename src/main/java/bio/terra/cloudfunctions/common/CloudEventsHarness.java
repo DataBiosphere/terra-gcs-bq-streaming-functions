4b2a47eec70db0a66628ee0ae28955d0e52df3f8
Ivan <ichang@broadinstitute.org>
2021-07-19 07:55:32 -0400

    Added base class App for processing business logic.

--- src/main/java/bio/terra/cloudfunctions/common/CloudEventsHarness.java
+++ src/main/java/bio/terra/cloudfunctions/common/CloudEventsHarness.java
@@ -9,23 +9,18 @@ import com.google.events.cloud.firestore.v1.DocumentEventData;
 import com.google.events.cloud.pubsub.v1.MessagePublishedData;
 import com.google.events.cloud.storage.v1.StorageObjectData;
 import io.cloudevents.CloudEvent;
-import io.cloudevents.core.v03.CloudEventV03;
-import io.cloudevents.core.v1.CloudEventV1;
 import java.util.logging.Logger;
-import org.openjdk.jol.vm.VM;
 
 public abstract class CloudEventsHarness implements CloudEventsFunction {
   private static final Logger logger = Logger.getLogger(CloudEventsHarness.class.getName());
 
-  protected Class<? extends CloudEvent> realization;
   protected CloudEvent event;
-  protected String messageTypeName;
   protected Object message;
 
   @Override
   public void accept(CloudEvent event) throws Exception {
     this.event = event;
-    this.realization = this.event.getClass();
+    parse();
   }
 
   public CloudEvent getEvent() {
@@ -36,49 +31,16 @@ public abstract class CloudEventsHarness implements CloudEventsFunction {
     return message;
   }
 
-  public <T> T getMessage(Class<T> clazz) {
-    return clazz.cast(message);
-  }
-
-  public boolean isCloudEventV1() {
-    logger.info("isCloudEventV1: " + VM.current().addressOf(CloudEventV1.class));
-    logger.info("isCloudEventV1: " + VM.current().addressOf(CloudEvent.class));
-    logger.info("isCloudEventV1: " + VM.current().addressOf(this.event.getClass()));
-    logger.info("hashCode: " + CloudEventV1.class.hashCode());
-    logger.info("hashCode: " + this.event.getClass().hashCode());
-    logger.info("isCloudEventV1: " + event.getClass().getTypeName());
-    logger.info("isCloudEventV1: " + event.getClass().getName());
-    logger.info("isCloudEventV1: " + event.getClass().getCanonicalName());
-    logger.info("isCloudEventV1: " + event.getClass().getSimpleName());
-    logger.info("isCloudEventV1: " + CloudEvent.class.isInstance(event));
-    logger.info("isCloudEventV1: " + CloudEventV1.class.isInstance(event));
-    logger.info(
-        "isCloudEventV1 assignable: " + CloudEvent.class.isAssignableFrom(event.getClass()));
-    logger.info(
-        "isCloudEventV1 assignable: " + CloudEventV1.class.isAssignableFrom(event.getClass()));
-
-    return CloudEventV1.class.isInstance(event);
-    // return CloudEventV1.class.getTypeName().equals(event.getClass().getTypeName());
-  }
-
-  public boolean isCloudEventV03() {
-    logger.info("isCloudEventV03: " + event.getClass().getTypeName());
-    logger.info("isCloudEventV03: " + CloudEventV03.class.isInstance(event));
-    logger.info("isCloudEventV03: " + event.getClass().isAssignableFrom(CloudEventV03.class));
-    return CloudEventV03.class.isInstance(event);
-    // return CloudEventV03.class.getTypeName().equals(event.getClass().getTypeName());
+  public CloudEventType getCloudEventType() {
+    return CloudEventType.fromCode(event.getType());
   }
 
   public void parse() throws Exception {
-    if (isCloudEventV1()) {
-      parseCloudEventV1();
-    } else if (isCloudEventV03()) {
-      parseCloudEventV03();
-    }
+    parseCloudEventV1();
   }
 
   private void parseCloudEventV1() throws Exception {
-    CloudEventType eventType = CloudEventType.fromCode(event.getType());
+    CloudEventType eventType = getCloudEventType();
     switch (eventType) {
       case FIRESTORE_DOCUMENT_V1_CREATED:
       case FIRESTORE_DOCUMENT_V1_DELETED:
@@ -88,14 +50,12 @@ public abstract class CloudEventsHarness implements CloudEventsFunction {
             new FirestoreEventMessage(
                 event.getData().toBytes(),
                 d -> GsonWrapper.getInstance().fromJson(new String(d), DocumentEventData.class));
-        messageTypeName = FirestoreEventMessage.class.getTypeName();
         break;
       case PUBSUB_TOPIC_V1_MESSAGE_PUBLISHED:
         message =
             new PubSubEventMessage(
                 event.getData().toBytes(),
                 d -> GsonWrapper.getInstance().fromJson(new String(d), MessagePublishedData.class));
-        messageTypeName = PubSubEventMessage.class.getTypeName();
         break;
       case STORAGE_OBJECT_V1_ARCHIVED:
       case STORAGE_OBJECT_V1_DELETED:
@@ -105,21 +65,16 @@ public abstract class CloudEventsHarness implements CloudEventsFunction {
             new StorageObjectEventMessage(
                 event.getData().toBytes(),
                 d -> GsonWrapper.getInstance().fromJson(new String(d), StorageObjectData.class));
-        StorageObjectEventMessage m = StorageObjectEventMessage.class.cast(message);
-        messageTypeName = StorageObjectEventMessage.class.getTypeName();
         logger.info(
-            "getGenericSuperclass: "
-                + StorageObjectEventMessage.class.getGenericSuperclass().getTypeName());
-        // logger.info(
-        //    "Concrete event data: "
-        //        + Class.forName(messageTypeName).asSubclass().cast(message).getMessage());
+            String.format("Event implementation class: %s", message.getClass().getTypeName()));
         break;
       default:
+        logger.severe(String.format("Unsupported event type '%s'.", event.getType()));
         break;
     }
   }
 
   private void parseCloudEventV03() {
-    throw new UnsupportedOperationException("parseCloudEventV03 method not yet supported");
+    throw new UnsupportedOperationException("Unsupported parseCloudEventV03 method.");
   }
 }
--- src/main/java/bio/terra/cloudfunctions/streaming/GenericCFEventHandler.java
+++ src/main/java/bio/terra/cloudfunctions/streaming/GenericCFEventHandler.java
@@ -1,32 +1,38 @@
 package bio.terra.cloudfunctions.streaming;
 
+import bio.terra.cloudevents.v1.CloudEventType;
+import bio.terra.cloudevents.v1.messagewrapper.StorageObjectEventMessage;
+import bio.terra.cloudfunctions.common.App;
 import bio.terra.cloudfunctions.common.CloudEventsHarness;
+import com.google.events.cloud.storage.v1.StorageObjectData;
 import io.cloudevents.CloudEvent;
-import io.cloudevents.core.v1.CloudEventV1;
 import java.util.logging.Logger;
 
 public class GenericCFEventHandler extends CloudEventsHarness {
   private static final Logger logger = Logger.getLogger(GenericCFEventHandler.class.getName());
+  private GenericApp app;
 
   @Override
   public void accept(CloudEvent event) throws Exception {
     super.accept(event);
-    parse();
-    logger.info("Type: " + event.getType());
-    logger.info("DataContentType: " + event.getDataContentType());
-    logger.info("Source: " + event.getSource().toString());
-    logger.info("TypeName: " + event.getClass().getTypeName());
-    logger.info("Test 1: " + CloudEventV1.class.isInstance(event));
-    logger.info(
-        "Test 2: " + (event.getClass().getTypeName().equals(CloudEventV1.class.getTypeName())));
-    logger.info("Test 3: " + CloudEventV1.class);
-    logger.info("Data Schema: " + event.getDataSchema());
-    if (CloudEventV1.class.isInstance(event)) {
-      if (event.getType().equals("google.cloud.storage.object.v1.finalized")) {
-        logger.info("Data TypeName: " + event.getData().getClass().getTypeName());
-        logger.info("Data Schema: " + event.getDataSchema().toString());
+    app = new GenericApp(getCloudEventType(), getMessage());
+    app.process();
+  }
+
+  class GenericApp extends App {
+    public GenericApp(CloudEventType eventType, Object message) {
+      super(eventType, message);
+    }
+
+    @Override
+    public void process() throws Exception {
+      if (CloudEventType.STORAGE_OBJECT_V1_FINALIZED.getCode().equals(eventType.getCode())) {
+        StorageObjectData data = ((StorageObjectEventMessage) getMessage()).getMessage();
+        logger.info(
+            String.format(
+                "Received %s event from bucket %s of content type %s",
+                getEventType().getDesc(), data.getBucket(), data.getContentType()));
       }
     }
-    logger.info("EventDataBytes: " + new String(event.getData().toBytes()));
   }
 }
