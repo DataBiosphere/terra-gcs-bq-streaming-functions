0e9d6ef8fcbfc7eeb9a41b36ba9d586c4322c9aa
Ivan <ichang@broadinstitute.org>
2021-07-19 11:34:44 -0400

    Make specific event message class final.

--- src/main/java/bio/terra/cloudevents/v1/messagewrapper/StorageObjectEventMessage.java
+++ src/main/java/bio/terra/cloudevents/v1/messagewrapper/StorageObjectEventMessage.java
@@ -2,7 +2,7 @@ package bio.terra.cloudevents.v1.messagewrapper;
 
 import com.google.events.cloud.storage.v1.StorageObjectData;
 
-public class StorageObjectEventMessage extends EventMessage<StorageObjectData> {
+public final class StorageObjectEventMessage extends EventMessage<StorageObjectData> {
   public StorageObjectEventMessage(byte[] value, ToTarget<StorageObjectData> mapper) {
     super(value, mapper);
   }
