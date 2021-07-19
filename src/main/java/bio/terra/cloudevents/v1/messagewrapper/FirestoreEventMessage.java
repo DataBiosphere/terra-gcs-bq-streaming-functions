c7340c883159bcce9069eca63e97b75dabb87ae5
Ivan <ichang@broadinstitute.org>
2021-07-19 11:34:44 -0400

    Make specific event message class final.

--- src/main/java/bio/terra/cloudevents/v1/messagewrapper/FirestoreEventMessage.java
+++ src/main/java/bio/terra/cloudevents/v1/messagewrapper/FirestoreEventMessage.java
@@ -2,7 +2,7 @@ package bio.terra.cloudevents.v1.messagewrapper;
 
 import com.google.events.cloud.firestore.v1.DocumentEventData;
 
-public class FirestoreEventMessage extends EventMessage<DocumentEventData> {
+public final class FirestoreEventMessage extends EventMessage<DocumentEventData> {
   public FirestoreEventMessage(byte[] value, ToTarget<DocumentEventData> mapper) {
     super(value, mapper);
   }
--- src/main/java/bio/terra/cloudevents/v1/messagewrapper/PubSubEventMessage.java
+++ src/main/java/bio/terra/cloudevents/v1/messagewrapper/PubSubEventMessage.java
@@ -2,7 +2,7 @@ package bio.terra.cloudevents.v1.messagewrapper;
 
 import com.google.events.cloud.pubsub.v1.MessagePublishedData;
 
-public class PubSubEventMessage extends EventMessage<MessagePublishedData> {
+public final class PubSubEventMessage extends EventMessage<MessagePublishedData> {
   public PubSubEventMessage(byte[] value, ToTarget<MessagePublishedData> mapper) {
     super(value, mapper);
   }
