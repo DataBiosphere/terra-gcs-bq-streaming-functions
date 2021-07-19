3252e5ab6badd1a32f1814069eaa3f1c7d97ec79
Ivan <ichang@broadinstitute.org>
2021-07-19 17:01:45 -0400

    Added no-arg, parameterized constructor and setter method.

--- src/main/java/bio/terra/cloudfunctions/proto/ProtoFunc.java
+++ src/main/java/bio/terra/cloudfunctions/proto/ProtoFunc.java
@@ -4,16 +4,27 @@ import bio.terra.cloudfunctions.common.CloudEventsHarness;
 import io.cloudevents.CloudEvent;
 import java.util.logging.Logger;
 
-/** This class is a subclass of CloudEventsHarness (i.e. a Cloud Function) with a custom App. */
+/**
+ * This class is a subclass of CloudEventsHarness (i.e. a Cloud Function) with a custom App.
+ *
+ * <p>The no-arg and setter method is for supporting Cloud Function initialization and for this
+ * function to be potentially used in a managed DI framework as a service.
+ */
 public class ProtoFunc extends CloudEventsHarness {
   private static final Logger logger = Logger.getLogger(ProtoFunc.class.getName());
 
   // Can be injected through DI framework (Spring or Java CDI).
   private ProtoApp app;
 
-  //public ProtoFunc(ProtoApp app) {
-  //  this.app = app;
-  //}
+  public ProtoFunc() {}
+
+  public ProtoFunc(ProtoApp app) {
+    this.app = app;
+  }
+
+  public void setApp(ProtoApp app) {
+    this.app = app;
+  }
 
   @Override
   public void accept(CloudEvent event) throws Exception {
