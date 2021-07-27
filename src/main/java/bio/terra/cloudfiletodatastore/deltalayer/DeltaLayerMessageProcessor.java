f7f838f6d8b90128be13e6f15e61423e360f35fb
Drew Hite <drew@drewhite.org>
2021-07-27 14:55:07 -0400

    multiple function implementations, write to big query

--- src/main/java/bio/terra/cloudfiletodatastore/deltalayer/DeltaLayerMessageProcessor.java
+++ src/main/java/bio/terra/cloudfiletodatastore/deltalayer/DeltaLayerMessageProcessor.java
@@ -3,9 +3,7 @@ package bio.terra.cloudfiletodatastore.deltalayer;
 import bio.terra.cloudfiletodatastore.*;
 import bio.terra.cloudfiletodatastore.deltalayer.model.BqInserts;
 import bio.terra.cloudfiletodatastore.deltalayer.model.PointCorrection;
-import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -27,11 +25,12 @@ public class DeltaLayerMessageProcessor extends MessageProcessor {
   @Override
   public void processMessage() {
     byte[] resourceBytes = resourceFetcher.fetchResource();
-    List<PointCorrection> pointCorrections = GsonConverter.convertToListofClass(resourceBytes, new TypeToken<ArrayList<PointCorrection>>() {});
+    List<PointCorrection> pointCorrections =
+        GsonConverter.convertToListofClass(
+            resourceBytes, new TypeToken<ArrayList<PointCorrection>>() {});
     logger.info(String.format("Length of point corrections is %s", pointCorrections.size()));
     List<BqInserts> inserts = bqGenerator.getInserts(pointCorrections);
     logger.info(String.format("Length of inserts is %s", inserts.size()));
     bigQueryWriter.insertRows(inserts);
   }
-
 }
