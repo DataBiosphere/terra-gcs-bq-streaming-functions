e01eed4e6ab7a8b60f4d08804b89bcc105ebf9d6
Ivan <ichang@broadinstitute.org>
2021-08-10 10:15:17 -0400

    Added logger to test.

--- src/test/java/bio/terra/GsonWrapperTest.java
+++ src/test/java/bio/terra/GsonWrapperTest.java
@@ -3,9 +3,12 @@ package bio.terra;
 import bio.terra.cloudfunctions.common.GsonWrapper;
 import bio.terra.common.BaseTest;
 import com.google.events.cloud.storage.v1.StorageObjectData;
+import java.util.logging.Logger;
 import org.junit.Test;
 
 public class GsonWrapperTest extends BaseTest {
+  private static final Logger logger = Logger.getLogger(GsonWrapperTest.class.getName());
+
   @Test
   public void parseStorageObjectData() {
     try {
@@ -13,6 +16,7 @@ public class GsonWrapperTest extends BaseTest {
           GsonWrapper.convertFromClass(MOCK_EVENT_GZIP, StorageObjectData.class);
       assertStorageObjectData(data);
     } catch (Exception e) {
+      logger.severe(e.getMessage());
     }
   }
 }
