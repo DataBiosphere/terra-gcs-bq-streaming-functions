8ca7f10132ebf59e9d282f462457346f0c57e238
Drew Hite <drew@drewhite.org>
2021-08-11 15:40:45 -0400

    David PR requests

--- src/main/java/bio/terra/cloudfiletodatastore/deltalayer/DeltaLayerFileUploadedMessageProcessor.java
+++ src/main/java/bio/terra/cloudfiletodatastore/deltalayer/DeltaLayerFileUploadedMessageProcessor.java
@@ -6,6 +6,7 @@ import bio.terra.cloudfiletodatastore.deltalayer.model.json.PointCorrectionReque
 import bio.terra.cloudfunctions.common.GsonWrapper;
 import com.google.cloud.bigquery.BigQuery;
 import java.util.List;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -36,22 +37,27 @@ public class DeltaLayerFileUploadedMessageProcessor extends MessageProcessor {
 
   @Override
   public void processMessage() {
-    byte[] resourceBytes = resourceFetcher.fetchResourceBytes();
-    PointCorrectionRequest pointCorrectionRequest =
-        GsonWrapper.convertFromClass(new String(resourceBytes), PointCorrectionRequest.class);
-    logger.info(
-        String.format(
-            "Length of deserialized point corrections is %s",
-            pointCorrectionRequest.getInserts().size()));
-    DeltaLayerBqInsertGenerator bqGenerator = new DeltaLayerBqInsertGenerator();
-    List<InsertData> inserts =
-        bqGenerator.getInserts(
-            pointCorrectionRequest.getInserts(),
-            pointCorrectionRequest.getInsertTimestamp(),
-            pointCorrectionRequest.getInsertId());
-    logger.info(String.format("Length of generated bq inserts is %s", inserts.size()));
-    PointCorrectionDestination destination = pointCorrectionRequest.getDestination();
-    deltaLayerBigQueryWriter.insertRows(
-        inserts, destination.getBqDataset(), destination.getDatasetProject(), bigQuery);
+    try {
+      byte[] resourceBytes = resourceFetcher.fetchResourceBytes();
+      PointCorrectionRequest pointCorrectionRequest =
+          GsonWrapper.convertFromClass(new String(resourceBytes), PointCorrectionRequest.class);
+      logger.info(
+          String.format(
+              "Length of deserialized point corrections is %s",
+              pointCorrectionRequest.getInserts().size()));
+      DeltaLayerBqInsertGenerator bqGenerator = new DeltaLayerBqInsertGenerator();
+      List<InsertData> inserts =
+          bqGenerator.getInserts(
+              pointCorrectionRequest.getInserts(),
+              pointCorrectionRequest.getInsertTimestamp(),
+              pointCorrectionRequest.getInsertId());
+      logger.info(String.format("Length of generated bq inserts is %s", inserts.size()));
+      PointCorrectionDestination destination = pointCorrectionRequest.getDestination();
+      deltaLayerBigQueryWriter.insertRows(
+          inserts, destination.getBqDataset(), destination.getDatasetProject(), bigQuery);
+    } catch (Exception e) {
+      logger.log(Level.SEVERE, "Doubly logging exception..", e);
+      throw e;
+    }
   }
 }
