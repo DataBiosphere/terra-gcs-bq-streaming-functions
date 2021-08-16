83a13ce18886d7fe576249636524be944a04ebc2
Ivan <ichang@broadinstitute.org>
2021-08-16 12:26:39 -0400

    Added fail statement.

--- src/test/java/bio/terra/StorageObjectEventHarnessTest.java
+++ src/test/java/bio/terra/StorageObjectEventHarnessTest.java
@@ -1,5 +1,7 @@
 package bio.terra;
 
+import static org.junit.Assert.fail;
+
 import bio.terra.common.BaseTest;
 import com.google.events.cloud.storage.v1.StorageObjectData;
 import org.junit.Test;
@@ -13,6 +15,7 @@ public class StorageObjectEventHarnessTest extends BaseTest {
       StorageObjectData message = instance.getMessage();
       assertStorageObjectData(message);
     } catch (Exception e) {
+      fail(e.getMessage());
     }
   }
 }
