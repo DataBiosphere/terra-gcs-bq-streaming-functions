545a37386dcf9b85fbab11a0cc35130cb9538e2c
Ivan <ichang@broadinstitute.org>
2021-08-16 22:20:39 -0400

    Test CloudEvent.

--- src/main/java/bio/terra/cloudfunctions/proto/ProtoFunc.java
+++ src/main/java/bio/terra/cloudfunctions/proto/ProtoFunc.java
@@ -1,5 +1,6 @@
 package bio.terra.cloudfunctions.proto;
 
+import bio.terra.cloudfunctions.common.CloudEventsHarness;
 import bio.terra.cloudfunctions.common.CloudStorageEventHarness;
 import java.util.logging.Logger;
 
@@ -9,7 +10,7 @@ import java.util.logging.Logger;
  * <p>The no-arg and setter method is for supporting Cloud Function initialization and for this
  * function to be potentially used in a managed DI framework as a service.
  */
-public class ProtoFunc extends CloudStorageEventHarness {
+public class ProtoFunc extends CloudEventsHarness {
   private static final Logger logger = Logger.getLogger(ProtoFunc.class.getName());
 
   // Can be injected through DI framework (Spring or Java CDI).
@@ -27,8 +28,8 @@ public class ProtoFunc extends CloudStorageEventHarness {
 
   @Override
   public void doAccept() throws Exception {
-    logger.info(getContext().eventType());
-    logger.info(getEvent().getBucket());
+    //logger.info(getContext().eventType());
+    //logger.info(getEvent().getBucket());
     // Implement business logic here.
   }
 }
