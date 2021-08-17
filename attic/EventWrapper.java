e13f854d367dd8df266370f61313cc7a780442db
Ivan <ichang@broadinstitute.org>
2021-08-17 12:28:40 -0400

    Moved files not used by this PR to attic.

---
+++ attic/EventWrapper.java
@@ -0,0 +1,17 @@
+package bio.terra.cloudfunctions.common;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class EventWrapper {
+    /**
+     * Parse an event String and returns the event represented by generic event type E.
+     *
+     * @param json the event String
+     * @param type the type of the event
+     * @return object representing event type E
+     */
+    public static <E> E parseEvent(String json, Class<E> type) {
+        assert (StringUtils.isNotBlank(json));
+        return GsonWrapper.getInstance().fromJson(json, type);
+    }
+}
