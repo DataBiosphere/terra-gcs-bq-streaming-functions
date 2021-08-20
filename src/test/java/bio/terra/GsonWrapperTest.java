0759bf58373beba9e465ff17b8192f10d731b332
Ivan <ichang@broadinstitute.org>
2021-08-19 23:14:54 -0400

    Removed try-catch.

--- src/test/java/bio/terra/GsonWrapperTest.java
+++ src/test/java/bio/terra/GsonWrapperTest.java
@@ -1,7 +1,5 @@
 package bio.terra;
 
-import static org.junit.Assert.fail;
-
 import bio.terra.cloudfunctions.common.GsonWrapper;
 import bio.terra.common.BaseTest;
 import com.google.events.cloud.storage.v1.StorageObjectData;
@@ -11,12 +9,7 @@ public class GsonWrapperTest extends BaseTest {
 
   @Test
   public void parseStorageObjectData() {
-    try {
-      StorageObjectData data =
-          GsonWrapper.convertFromClass(MOCK_EVENT_GZIP, StorageObjectData.class);
-      assertMockTGZStorageObjectData(data);
-    } catch (Exception e) {
-      fail(e.getMessage());
-    }
+    StorageObjectData data = GsonWrapper.convertFromClass(MOCK_EVENT_GZIP, StorageObjectData.class);
+    assertMockTGZStorageObjectData(data);
   }
 }
