05e30a432d517a6fcb41ed280a800c43f71e2c55
Ivan <ichang@broadinstitute.org>
2021-08-20 17:02:14 -0400

    Moved ProtoApp, ProtoFunc and build=proto-cf-handler.yml to README.

---
+++ attic/ProtoFunc.java
@@ -0,0 +1,52 @@
+package bio.terra.cloudfiletodatastore.proto;
+
+import bio.terra.cloudfiletodatastore.FileUploadedMessage;
+import bio.terra.cloudfunctions.common.GoogleCloudEventHarness;
+import bio.terra.cloudfunctions.common.MediaTypeWrapper;
+import com.google.events.cloud.storage.v1.StorageObjectData;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * This class is a subclass of CloudEventsHarness (i.e. a Cloud Function) with a custom App.
+ *
+ * <p>The no-arg and setter method is for supporting Cloud Function initialization and for this
+ * function to be potentially used in a managed DI framework as a service.
+ */
+public class ProtoFunc extends GoogleCloudEventHarness {
+    private static final Logger logger = Logger.getLogger(ProtoFunc.class.getName());
+
+    @Override
+    public void doAccept() {
+        try {
+            String expectedBucket = System.getenv("GOOGLE_BUCKET");
+            StorageObjectData event = getEvent(StorageObjectData.class);
+            MediaTypeWrapper mediaType = new MediaTypeWrapper(event.getContentType());
+            if (isGoogleStorageObjectFinalize()
+                    && expectedBucket.equals(event.getBucket())
+                    && mediaType.isApplicationGzip()) {
+                // App can be injected through DI framework (Spring or Java CDI).
+                FileUploadedMessage fileUploadedMessage =
+                        new FileUploadedMessage(
+                                event.getName(), event.getBucket(), event.getSize(), event.getTimeCreated());
+                ProtoApp app = new ProtoApp(fileUploadedMessage);
+                app.process();
+            } else {
+                logger.log(
+                        Level.SEVERE,
+                        String.format(
+                                "Malformed event data: Expected %s event from bucket %s of content type %s but received %s event from bucket %s of content type %s",
+                                GoogleCloudEventHarness.GOOGLE_STORAGE_OBJECT_FINALIZE,
+                                expectedBucket,
+                                "application/gzip",
+                                getContext().eventType(),
+                                event.getBucket(),
+                                event.getContentType()));
+                return;
+            }
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "An unexpected error occurred.", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
