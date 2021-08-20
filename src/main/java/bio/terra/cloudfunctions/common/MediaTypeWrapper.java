53243a9a71decc8777de2004f1fc65d33ae1cce5
Ivan <ichang@broadinstitute.org>
2021-08-20 12:18:09 -0400

    Added comments to MediaTypeWrapper.

--- src/main/java/bio/terra/cloudfunctions/common/MediaTypeWrapper.java
+++ src/main/java/bio/terra/cloudfunctions/common/MediaTypeWrapper.java
@@ -4,6 +4,7 @@ import com.google.common.net.MediaType;
 
 public class MediaTypeWrapper {
   // Workaround for Mime types not found in Guava MediaType
+  // See issue https://github.com/google/guava/issues/3946
   public static final MediaType APPLICATION_GZIP = MediaType.create(MediaType.GZIP.type(), "gzip");
   public static final MediaType APPLICATION_JSON =
       MediaType.create(MediaType.JSON_UTF_8.type(), "json");
