3c6300a1b251e3c5ca908c5b1f9b4efbd349ef7f
Ivan <ichang@broadinstitute.org>
2021-08-19 23:10:37 -0400

    Removed unused logger.

--- src/test/java/bio/terra/GsonWrapperTest.java
+++ src/test/java/bio/terra/GsonWrapperTest.java
@@ -5,11 +5,9 @@ import static org.junit.Assert.fail;
 import bio.terra.cloudfunctions.common.GsonWrapper;
 import bio.terra.common.BaseTest;
 import com.google.events.cloud.storage.v1.StorageObjectData;
-import java.util.logging.Logger;
 import org.junit.Test;
 
 public class GsonWrapperTest extends BaseTest {
-  private static final Logger logger = Logger.getLogger(GsonWrapperTest.class.getName());
 
   @Test
   public void parseStorageObjectData() {
